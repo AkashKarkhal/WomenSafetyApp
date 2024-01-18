40bb08a36589525587d18586db5f73250592b376
Akash Karkhal <akashkarkhal.23@gmail.com>
2024-01-19 01:25:17 +0530

    splash screeen

---
+++ app/src/main/java/com/example/s/Activities/Splashscreen.java
@@ -0,0 +1,45 @@
+package com.example.s.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.s.R;
+
+public class Splashscreen extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        imageView = findViewById(R.id.img);
+
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(2500); // Adjust the duration as needed
+
+        // Set up an animation set if you need multiple animations
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(fadeIn);
+
+        imageView.startAnimation(animationSet);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        },3500);
+
+
+    }
+}
